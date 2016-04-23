b9e2b8c1c67070a3ca2e05954fd246eccf1819a1
Cristòfol Torrens <piffall@gmail.com>
2016-04-23 10:30:16 +0200

    Using lambdas

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
 
 // Event based steam screensaver unlocker
+var fs = require('fs');
 var exec = require('child_process').exec;
 var events = require('events');
 var fileTailer = require('file-tail');
-
-// Deamonize
-require('daemon')()
 
 // Start event loop.
 var emitter = new events.EventEmitter();
@@ -16,23 +14,31 @@ var defaultSteamHome = process.env.HOME + "/.steam";
 var streamLogFile = "/steam/logs/streaming_log.txt";
 var logFile = defaultSteamHome + streamLogFile;
 
-// Test with this file
+// Run or test
 if (process.argv[2] == "test") {
-  logFile = 'logfile.log';
+  logFile = '/tmp/monitor_example_file.log';
+  setInterval(() => {
+    fs.appendFile(logFile, "Example data line", (err) => {
+      console.log("Writting in file...");
+    });
+  },1000);
+} else {
+  require('daemon')()
 }
+
 
 // Show some output to user
 console.log("Monitoring log file: " + logFile);
 
 // Tail the file
 ft = fileTailer.startTailing(logFile);
-ft.on('line', function(line){
+ft.on('line', (line) => {
   emitter.emit("newline");
 });
 
 // Handle new line
-emitter.on("newline",function(){
-  exec("DISPLAY=:0 gnome-screensaver-command -d", function(error, stdout, stderr){
+emitter.on("newline",() => {
+  exec("DISPLAY=:0 gnome-screensaver-command -d", (error, stdout, stderr) => {
     if (error) {
       console.log("ERROR: " + stderr);
     } else {
